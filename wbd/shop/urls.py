4dd041bd73ca79957fe99d114644e6264642281d
pgaska <pawegska@ymail.com>
2019-01-12 18:58:28 +0100

    super strona logowania i test wypisywania obiektu

--- wbd/shop/urls.py
+++ wbd/shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hurtownia/', views.hurtownia, name='hurtownia'),
 ]
--- wbd/shop/views.py
+++ wbd/shop/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Hurtownie, Poczta
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'shop/index.html')
+
+def hurtownia(request, hurtownia_id=1):
+    hurtownia = get_object_or_404(Hurtownie, pk=hurtownia_id)
+    return render(request, 'shop/hurtownia.html', {'hurtownia': hurtownia})
