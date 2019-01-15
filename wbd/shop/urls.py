74f7ba3c860c6f62a4db425c04a30c57e625f6dc
pgaska <pawegska@ymail.com>
2019-01-15 17:55:20 +0100

    dodanie logowania i poczatek szablonu wyszukiwania

--- wbd/shop/urls.py
+++ wbd/shop/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hurtownia/', views.hurtownia, name='hurtownia'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('magazines/', views.magazines, name='magazines'),
+    path('workers/', views.workers, name='workers'),
+    path('goods/', views.goods, name='goods')
 ]
--- wbd/shop/views.py
+++ wbd/shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Hurtownie, Poczta
 
@@ -8,5 +10,34 @@ def index(request):
     return render(request, 'shop/index.html')
 
 def hurtownia(request, hurtownia_id=1):
-    hurtownia = get_object_or_404(Hurtownie, pk=hurtownia_id)
-    return render(request, 'shop/hurtownia.html', {'hurtownia': hurtownia})
+    if not request.user.is_authenticated:
+        return redirect(index)
+
+    else:
+        hurtownia = get_object_or_404(Hurtownie, pk=hurtownia_id)
+        return render(request, 'shop/hurtownia.html', {'hurtownia': hurtownia})
+
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(hurtownia)
+
+    else:
+        return redirect(index)
+
+def logout_view(request):
+    logout(request)
+    return redirect(index)
+
+def magazines(request):
+    return render(request, 'shop/magazines.html')
+
+def workers(request):
+    pass
+
+def goods(request):
+    pass
